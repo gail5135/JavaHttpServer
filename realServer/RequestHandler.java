dcb82a4fb582c88442e7cc02f2add663fa9bf3f3
gail5135 <gail5153@gmail.com>
2020-02-21 14:31:06 +0900

    2020-02-22 | GET 90% complete

--- realServer/RequestHandler.java
+++ realServer/RequestHandler.java
@@ -11,26 +11,25 @@ import java.util.HashMap;
 
 public class RequestHandler {
     // Field
-    private Path BAD_REQUEST_HTML_PATH = Paths.get("resources","400.html");
-    private Path FORBIDDEN_HTML_PATH = Paths.get("resources/403.html");
-    private Path NOT_FOUND_HTML_PATH = Paths.get("resources/404.html");
+    private Path BAD_REQUEST_HTML_PATH = Paths.get("src/realServer/resources/400.html");
+    private Path FORBIDDEN_HTML_PATH = Paths.get("src/realServer/resources/403.html");
+    private Path NOT_FOUND_HTML_PATH = Paths.get("src/realServer/resources/404.html");
     private Request request;
-    private static String HTML_MIME = "text/html;charset=utf8";
+    private String HTML_MIME = "text/html;charset=utf8";
     private MIME mime;
 
     // Method
     public Response handleRequest(Request request) throws IOException{
-        System.out.println(this.getClass().getResource("").getPath()+BAD_REQUEST_HTML_PATH);
+        System.out.println(request.getPath());
 
-
+        System.out.println(MyServer.RESPONSE_RESOURCE);
         if(request == null){
             return new Response(Status.BAD_REQUEST, HTML_MIME, Files.readAllBytes(BAD_REQUEST_HTML_PATH));
         }
 
-        Path resourcePath = Paths.get("./resources/", request.getPath()).normalize();
-        System.out.println(Files.readAllBytes(resourcePath).toString());
+        Path resourcePath = Paths.get("resources/helloWorld.html");
 
-        if(!resourcePath.startsWith("./resources/")){
+        if(!resourcePath.startsWith("resources")){
             return new Response(Status.FORBIDDEN, HTML_MIME, Files.readAllBytes(FORBIDDEN_HTML_PATH));
         }
 
@@ -39,7 +38,7 @@ public class RequestHandler {
             return new Response(Status.OK, mime, Files.readAllBytes(resourcePath));
         }
 
-        return null;
+        return new Response(Status.NOT_FOUND, HTML_MIME, Files.readAllBytes(NOT_FOUND_HTML_PATH.toAbsolutePath()));
     }
 
     public MIME getMIME(Path path){
